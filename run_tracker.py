7469e256531d732d59668d5de2cb53c7e2d6866f
Andy <andy@idfa.cc>
2026-05-08 09:28:26 +0200

    created version to be configured as windows startup script instead of windows scheduled task

--- run_tracker.py
+++ run_tracker.py
@@ -1,7 +1,13 @@
 from tracker.main import main
 from shared import defaults
 import logging
+import time
 
 if __name__ == "__main__":
     defaults.configure_logging()
-    main()
+    while True:
+        try:
+            main()
+        except Exception as e:
+            logging.error(f"Error: {e}")
+        time.sleep(defaults.service_check_interval*60)
